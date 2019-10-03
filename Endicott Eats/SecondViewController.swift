8d2935018da1de9a65705a88c6d26b7f82c4e437
stevesuchcicki <ssuch855@mail.endicott.edu>
2019-10-03 13:00:39 -0400

    Initial Commit

---
+++ Endicott Eats/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Endicott Eats
+//
+//  Created by Steve Suchcicki on 10/3/19.
+//  Copyright © 2019 Steve Suchcicki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

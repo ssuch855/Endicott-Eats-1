683eca4591dd8a2057ec96f6fa546fb673f61337
stevesuchcicki <ssuch855@mail.endicott.edu>
2019-12-12 08:25:54 -0500

    Added Dining Options List

--- Endicott Eats/OrderViewController.swift
+++ Endicott Eats/OrderViewController.swift
@@ -9,11 +9,63 @@
 import UIKit
 import Firebase
 
-class OrderViewController : UIViewController {
-
+class OrderViewController : UITableViewController {
+    
+    
+    var Callahan: [String:Any] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "callahanCell", for: indexPath)
         
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "goToMenu", sender: self)
     }
     
 }
+
+
+
+//        let db = Firestore.firestore()
+//
+//        db.collection("menus").document("Callahan").getDocument { (document, error) in
+//            if error == nil && document != nil {
+//                self.Callahan = document!.data()!
+//            }
+//        }
+//
+//        print(Callahan)
+        
+//        db.collection("menus").document("Callahan").getDocument { (document, error) in
+//            if error == nil && document != nil {
+//                //let Callahan = document!.data()
+//            }
+//        }
+//
+//        db.collection("menus").document("Callahan").getDocument { (document, error) in
+//            if error == nil && document != nil {
+//                let Callahan = document!.data()
+//            }
+//        }
+        
+
+        
+//        db.collection("menus").document("Callahan").getDocument { (document, error) in
+//
+//            if error == nil && document != nil {
+//
+//                var callahanData: [String:Any] = document!.data()
+//                    print(documentData)
+//
+//            }
+//        }
